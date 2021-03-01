47bbeb4d2dd4bcee41b110d993f563d63f4888c4
stsharin <stssharin@gmail.com>
2021-03-01 23:42:40 +0600

    added style

--- src/components/Selection/Selection.js
+++ src/components/Selection/Selection.js
@@ -10,14 +10,37 @@ const Selection = (props) => {
         const selected = selection[i];
         totalSelectedName = totalSelectedName + " " + selected.name;
         selectedPlayerSalary = selectedPlayerSalary + " " + selected.salary;
+
     }
 
     return (
-        <div>
-            <h3>Selected: {selection.length}</h3>
-            <h4>Total Budget: {totalSalary}</h4>
-            <h3>Selected Player Name: {totalSelectedName}</h3>
-            <h3>Selected Player Salary: {selectedPlayerSalary}</h3>
+        <div className="container">
+            <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+                <div class="col m-2">
+                    <div class="p-3 border bg-light">
+                        <h3>Selected: {selection.length}</h3>
+                    </div>
+                </div>
+                <div class="col m-2">
+                    <div class="p-3 border bg-light">
+                        <h4>Total Budget: {totalSalary}</h4>
+                    </div>
+                </div>
+                <div class="col m-2">
+                    <div class="p-3 border bg-light">
+                    <h3 style={{color: "green"}}>Selected Player Name: </h3>
+                    
+                    <h5>{totalSelectedName}</h5>
+                    </div>
+                </div>
+                <div class="col m-2">
+                    <div class="p-3 border bg-light">
+                        <h3 style={{color: "green"}}>Selected Player Salary: </h3>
+                        <h4>{selectedPlayerSalary}</h4>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
